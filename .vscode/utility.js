806ed57f23d4c7a9a18a90b0c7e632afe5a89735
soham874 <soham874@gmail.com>
2020-12-21 14:40:29 +0530

    Added : Half Day Wage calculations

--- .vscode/utility.js
+++ .vscode/utility.js
@@ -1,21 +1,26 @@
-class Utility{
-    isPresentorAbsent(){
-        
+class Utility {
+    isPresentorAbsent() {
+
         let IS_FULL_DAY = 8
-        
-        let attendance = Math.floor(Math.random() * 10 % 2)
+        let IS_HALF_DAY = 4
+
+        let attendance = Math.floor(Math.random() * 10 % 3)
         if (attendance === 0)
             console.log("Employee is absent.")
+        else if (attendance === 1) {
+            attendance = IS_HALF_DAY
+            console.log("Employee is present half day.")
+        }
         else {
             attendance = IS_FULL_DAY
-            console.log("Employee is present.")
+            console.log("Employee is present full day.")
         }
-            
-        
+
+
         return attendance
     }
 
-    calculateEmpWage(){
+    calculateEmpWage() {
         let DAILY_WAGE = 20
         let workhours = DAILY_WAGE * this.isPresentorAbsent()
         console.log("Employee wage is " + workhours)
